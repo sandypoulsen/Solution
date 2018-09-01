8886379f0ab59b3e6f4174a84bf3b039d2c47dd0
sandypoulsen <sandypoulsen@gmail.com>
2018-09-01 13:26:26 +0200

    01-09-2018

---
+++ DemoApp13/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace DemoApp13
+{
+    public  class MainClass
+    {
+        public static ThreadLocal<int> _field =
+            new ThreadLocal<int>(() =>
+        {
+            return Thread.CurrentThread.ManagedThreadId;
+        });
+
+        public static void Main(string[] args)
+        {
+            new Thread(() =>
+            {
+                for (int x = 0; x < _field.Value; x++ )
+                {
+                    Console.WriteLine("Therad A: {0}", x);
+                }
+
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int x = 0; x < _field.Value; x++)
+                {
+                    Console.WriteLine("Therad B: {0}", x);
+                }
+            }).Start();
+
+            ThreadPool.QueueUserWorkItem((s) => 
+            { 
+                Console.WriteLine("Working on a thread from threadpool");
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DemoApp14/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DemoApp14
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 42;
+            }).ContinueWith((i) => i.Result * 2, TaskContinuationOptions.OnlyOnRanToCompletion);
+            Console.WriteLine(t.Result);
+        }
+    }
+}
